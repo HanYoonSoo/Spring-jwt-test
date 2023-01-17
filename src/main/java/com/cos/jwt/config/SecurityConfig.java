ea83da58c107622bf6b28a12ed4b236e030e9a53
HanYoonSoo <hdbstn3055@gmail.com>
2023-01-17 20:31:45 +0900

    First commit

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,2 +1,44 @@
-package com.cos.jwt.config;public class SecurityConfig {
+package com.cos.jwt.config;
+
+import com.cos.jwt.filter.MyFilter1;
+import com.cos.jwt.filter.MyFilter3;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.context.SecurityContextHolderFilter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsFilter corsFilter;
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception{
+        http.addFilterBefore(new MyFilter3(), SecurityContextHolderFilter.class);
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+                .addFilter(corsFilter) // @CrossOrigin(인증X), 시큐리티 필터에 등록 인증(O)
+                .formLogin().disable()
+                .httpBasic().disable()  // 요청할 때 마다 인증하는 방식 -> HTTPS 서버에서는 괜찮겠지만 우리는 Token을 넣어서 쓰는 방식을 사용하기 때문에 disable() 한다.
+                .authorizeRequests()
+                .antMatchers("/api/v1/user/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+        return http.build();
+    }
 }
--- src/main/java/com/cos/jwt/filter/MyFilter2.java
+++ src/main/java/com/cos/jwt/filter/MyFilter2.java
@@ -3,10 +3,10 @@ package com.cos.jwt.filter;
 import javax.servlet.*;
 import java.io.IOException;
 
-public class MyFilter1 implements Filter {
+public class MyFilter2 implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("필터1");
+        System.out.println("필터2");
         filterChain.doFilter(servletRequest, servletResponse);
     }
 }
